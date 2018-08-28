9b9b7704da82a7e3716671a7125fc7d6789e31d5
Dani Domínguez <dani.d.zamorano@gmail.com>
2018-08-28 17:51:49 +0200

    add jest tests

--- src/app.js
+++ src/app.js
@@ -12,15 +12,6 @@ import './styles/style.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'water bill', amount: 100, createdAt: 100 }));
-store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: 500 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 312444, createdAt: 200 }));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
